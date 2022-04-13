8e00cf9a3dde70ea2025c3909839b2e985269f2e
Demetrius Magela da Mata <63015481+demetriusengdados@users.noreply.github.com>
2022-04-13 11:35:06 -0300

    Add files via upload

---
+++ Listando arquivos dentro do arquivo.sh
@@ -0,0 +1,15 @@
+$ unzip -l work.zip
+Archive:  work.zip
+  #Length      Date    Time    Name
+#---------  ---------- -----   ----
+        #0  2016-01-16 19:36   workspace/
+        #0  2016-01-16 20:34   workspace/projetos-c#/
+       #10  2016-01-16 19:08   workspace/projetos-c#/bemvindo.txt
+        #0  2016-01-16 20:34   workspace/projetos-php/
+       #10  2016-01-16 19:08   workspace/projetos-php/bemvindo.txt
+        #0  2016-01-16 20:34   workspace/projetos-java/
+       #10  2016-01-16 19:08   workspace/projetos-java/bemvindo.txt
+       #10  2016-01-16 19:08   workspace/bemvindo.txt
+       #10  2016-01-16 19:07   workspace/bemvindo2.txt
+#---------                     -------
+       #50                     9 files
