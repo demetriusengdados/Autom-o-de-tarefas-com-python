8335ab93b041b9949235d0723924e6f17ead0c05
Demetrius Magela da Mata <63015481+demetriusengdados@users.noreply.github.com>
2022-09-02 08:45:34 -0300

    Add files via upload

--- script are3d.py
+++ script are3d.py
@@ -1,6 +1,6 @@
 from openpyxl import Workbook
 from openpyxl.chart import (
-    AreaChart,
+    AreaChart3D,
     Reference,
     Series,
 )
@@ -11,25 +11,27 @@ ws = wb.active
 # Dados da contabilidade em %
 rows = [
     ['Ano', 'Lucro', 'Custos'],
-    [2015, 40, 30],
-    [2016, 40, 25],
-    [2018, 50, 30],
-    [2019, 30, 10],
-    [2020, 25, 5]
+    [2015, 30, 40],
+    [2016, 25, 40],
+    [2017, 30, 50],
+    [2018, 10, 30],
+    [2019, 5, 25],
+    [2020, 10, 50],
 ]
 
 for row in rows:
     ws.append(row)
 
-chart = AreaChart()
+chart = AreaChart3D()
 chart.title = "Lucros x Custos"
 chart.style = 13
 chart.x_axis.title = 'Ano'
 chart.y_axis.title = 'Porcentagem'
+chart.legend = None
 
 categorias = Reference(ws, min_col=1, min_row=1, max_row=6)
 dados = Reference(ws, min_col=2, min_row=1, max_col=3, max_row=6)
-chart.add_data(dados, titles_from_data=False)
+chart.add_data(dados, titles_from_data=True)
 chart.set_categories(categorias)
 
 ws.add_chart(chart, "A10")
